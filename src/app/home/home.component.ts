0b95b5ed8e8197212e8b2f5aad11c528d3095f71
Nic Small <nic@element84.com>
2020-07-08 17:07:03 -0400

    Added canvas js

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../canvasjs.min.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,30 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const dataPoints = [
+      { y: 71 },
+      { y: 55 },
+      { y: 50 },
+      { y: 65 },
+      { y: 95 },
+      { y: 68 },
+      { y: 28 },
+      { y: 34 },
+      { y: 14 }
+    ];
+
+    const chart = new CanvasJS.Chart('chartContainer', {
+      animationEnabled: true,
+      title: {
+        text: 'Basic Column Chart'
+      },
+      data: [{
+        type: 'column',
+        dataPoints
+      }]
+    });
+
+    chart.render();
   }
 
 }
