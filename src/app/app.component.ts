6b42acfb445f37658a4258836217088dd2eeecbf
Nic Small <nic@element84.com>
2020-07-08 16:42:06 -0400

    Hello world. Added home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'health-patterns-ui';
+  title = 'My Health Patterns';
 }
